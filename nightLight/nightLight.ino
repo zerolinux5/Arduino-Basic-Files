74c51c319adb653fc847904174f0d447aebf601a
Jesus Magana <maganazero5@gmail.com>
2014-10-15 11:46:34 -0700

    Use a digital step value

--- nightLight/nightLight.ino
+++ nightLight/nightLight.ino
@@ -2,8 +2,9 @@ const int RLED = 11;
 const int GLED = 10;
 const int BLED = 9;
 const int LIGHT = 0;
-const int MIN_LIGHT=200;
-const int MAX_LIGHT=900;
+const int STAGE_1=100;
+const int STAGE_2=300;
+const int STAGE_3=1000;
 
 
 int val = 0;
@@ -18,8 +19,24 @@ void setup()
 
 void loop()
 {
-  val = analogRead(TMP);
+  val = analogRead(LIGHT);
   Serial.println(val);
+  if(val <= STAGE_1)
+  {
+    analogWrite(RLED, 255);
+    analogWrite(GLED, 255);
+    analogWrite(BLED, 255);
+  }else if( val <= STAGE_2)
+  {
+    analogWrite(RLED, 127);
+    analogWrite(GLED, 127);
+    analogWrite(BLED, 127);
+  } else 
+  {
+    digitalWrite(RLED, LOW);
+    digitalWrite(GLED, LOW);
+    digitalWrite(BLED, LOW);
+  }  
 }
     
     
